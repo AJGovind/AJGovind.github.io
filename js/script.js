c8136323c2719f4d06df1f70790ec988e02ba64b
jayatumithiladham <117332615+AJGovind@users.noreply.github.com>
2023-01-09 15:09:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.type', {
+    strings:
+      ["Developer.",
+        "Cool.",
+        "Handsome.",
+        "Student.",
+        "Web Developer"
+      ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+  });
